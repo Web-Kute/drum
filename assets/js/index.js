a7a951014a142222d0bc78a6ce8885a7e78b2993
Ikigai <alvardsandz@gmail.com>
2024-05-16 19:11:49 +0200

    add random color

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,33 @@
 window.addEventListener('keydown', playSound);
 
-function playSound(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+function playSound(event) {
+  const audio = document.querySelector(`audio[data-key="${event.key}"]`);
+  const key = document.querySelector(`.key[data-key="${event.key}"]`);
   if (key && audio) {
     audio.play();
-    key.classList.add('playing');
     audio.currentTime = 0;
+
+    key.classList.add('playing');
+    key.children[0].style.backgroundColor =
+      colors[Math.floor(Math.random() * colors.length)];
+    key.children[0].style.color = '#FFF';
+
+    setTimeout(() => {
+      key.classList.remove('playing');
+      key.children[0].style.backgroundColor = '#EEE';
+      key.children[0].style.color = '#000';
+    }, 200);
   }
-  setTimeout(() => key.classList.remove('playing'), 200);
 }
+
+const colors = [
+  '#ef5777',
+  '#575fcf',
+  '#ffc048',
+  '#ffa801',
+  '#ffd32a',
+  '#1e272e',
+  '#05c46b',
+  '#00d8d6',
+  '#0fbcf9',
+];
